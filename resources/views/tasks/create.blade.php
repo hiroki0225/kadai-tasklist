52675385531dc495ae0b8be47b080d7ea8c10bad
EC2 Default User <ec2-user@ip-172-31-4-147.us-east-2.compute.internal>
2018-05-23 09:22:25 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,82 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+    
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+     <div class="form-group">
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+
+    <div class="form-group">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
+
+        {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+     </div>
+     </div>
+
+<!--
+     <div class="col-sm-offset-2 col-sm-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+     <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
 
+    <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
 
-        {!! Form::submit('登録') !!}
+        {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
 
     {!! Form::close() !!}
+     </div>
+     
+     
+     <div class="col-md-offset-2 col-md-6">
 
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+     <div class="form-group">
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+
+    <div class="form-group">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
+
+        {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+     </div>
+     
+     
+     <div class="col-lg-offset-3 col-md-3">
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+     <div class="form-group">
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+
+    <div class="form-group">
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+     </div>
+
+        {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+
+    {!! Form::close() !!}
+     </div>
+     -->
+</div>
 @endsection
